91b6e1ee6399c32ab7da874ec6862f73e45e3834
Mathis <70844521+Batmath@users.noreply.github.com>
2025-03-20 17:20:12 +0100

    Ajout du fichier de l'interface ainsi que des imports nécessaire à son fonctionnement

---
+++ IHM.py
@@ -0,0 +1,12 @@
+from customtkinter import *
+import tkinter as tk
+from CTkListbox import *
+
+#pip install customtkinter
+#pip install CTkListbox
+#pip install tkinter
+
+class IHM():
+
+    def __init__(self):
+        return
