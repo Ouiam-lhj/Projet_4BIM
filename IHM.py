d48973aea9b9753d36ae6637a7c62119cd472f67
Mathis <70844521+Batmath@users.noreply.github.com>
2025-03-20 17:32:23 +0100

    Ajout du code de base de création de l'interface graphique avec la fenêtre principale et le bouton amenant au formulaire

--- IHM.py
+++ IHM.py
@@ -9,4 +9,26 @@ from CTkListbox import *
 class IHM():
 
     def __init__(self):
+        self.root = CTk()
+        self.homePage()
+        self.root.mainloop()
+
+    def homePage(self):
+
+        self.root.title("Le profiler des zencoders")
+        self.principalMainframe = CTkFrame(self.root, fg_color="#38393b", border_width = 0)
+        self.principalMainframe.pack(expand=True, fill="both", side="right")
+
+        self.menuMainframe = CTkFrame(self.root, width= 200)
+        self.menuMainframe.pack(fill="y", side="left")
+
+        self.menuProfilTabButton = CTkButton(self.menuMainframe, text="Formulaire", command= lambda : self.displayFormulaire(), fg_color="transparent", border_width = 0, hover_color=['#e4e4eb', '#3a3b3d'])
+
+        self.menuProfilTabButton.pack(fill="x", pady=10)
+    
+    def displayFormulaire(self):
+        print("Code du formulaire ici")
         return
+
+if __name__ == "__main__":
+    test = IHM()
