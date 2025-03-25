8fd0f5d05b42f6d8624995732f19628e284cb9ea
Thamis-First <batangamathis@hotmail.com>
2025-03-25 14:32:25 +0100

    Modification de la structure de la méthode homePage, ajout des différentes Frames principal ainsi que des colorations pour la visualisation

--- IHM.py
+++ IHM.py
@@ -16,15 +16,22 @@ class IHM():
     def homePage(self):
 
         self.root.title("Le profiler des zencoders")
+
         self.principalMainframe = CTkFrame(self.root, fg_color="#38393b", border_width = 0)
-        self.principalMainframe.pack(expand=True, fill="both", side="right")
-
         self.menuMainframe = CTkFrame(self.root, width= 200)
+        self.titleFrame = CTkFrame(self.principalMainframe, fg_color="#00FF00", height = 70)
+        self.photosFrame = CTkFrame(self.principalMainframe, fg_color="#0000FF", height = 400)
+        self.buttonsFrame=CTkFrame(self.principalMainframe, fg_color="#FF0000", height = 50)
+
+        self.menuFormButton = CTkButton(self.menuMainframe, text="Formulaire", command= lambda : print(self.root.winfo_geometry()), fg_color="transparent", border_width = 0, hover_color=['#e4e4eb', '#3a3b3d'])
+        
+        self.principalMainframe.pack(expand=True, fill="both", side="right")
         self.menuMainframe.pack(fill="y", side="left")
+        self.titleFrame.pack(expand=True, fill="both", side="top")
+        self.photosFrame.pack(expand=True, fill="both", side="top")
+        self.buttonsFrame.pack(expand=True, fill="both", side="top")
 
-        self.menuProfilTabButton = CTkButton(self.menuMainframe, text="Formulaire", command= lambda : self.displayFormulaire(), fg_color="transparent", border_width = 0, hover_color=['#e4e4eb', '#3a3b3d'])
-
-        self.menuProfilTabButton.pack(fill="x", pady=10)
+        self.menuFormButton.pack(fill="x", pady=10)
     
     def displayFormulaire(self):
         print("Code du formulaire ici")
